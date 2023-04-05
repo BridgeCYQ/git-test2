3fa081c6116fcd5f435115fc103fabe31a7cfd77
Bridge <Brdige@email.com>
2023-04-05 17:48:42 +0800

    second commit

--- src/main/java/com/atguigu/GitTest.java
+++ src/main/java/com/atguigu/GitTest.java
@@ -3,5 +3,8 @@ package com.atguigu;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello Git");
+        System.out.println("Hello Git");
+        System.out.println("Hello Git");
+        System.out.println("Hello Git");
     }
 }
